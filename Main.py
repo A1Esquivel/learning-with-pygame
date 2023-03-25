b6d6c79e26fee4ac13c4b8307a7351a9be44e2aa
Anthony Esquivel <lawtay90@gmail.com>
2023-03-24 23:18:18 -0500

    Added somee colors, refactored code to be more consistent.

--- Main.py
+++ Main.py
@@ -7,13 +7,17 @@ pygame.display.set_caption('Corvids Adventure')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('fonts/Pixeltype.ttf', 50)
 
-sky_surface = pygame.image.load('Graphics/Sky.png').convert()
-ground_surface = pygame.image.load('Graphics/Ground.png').convert()
-text_surface = test_font.render('Corvids Adventure', False, 'Black')
+sky_surf = pygame.image.load('Graphics/Sky.png').convert()
+ground_surf = pygame.image.load('Graphics/Ground.png').convert()
 
-snail_surface = pygame.image.load('Graphics/snail/snail1.png').convert_alpha()
-snail_x_pos = 600
-snail_y_pos = 250
+score_surf = test_font.render('Corvids Adventure', False, (64,64,64))
+score_rect = score_surf.get_rect(center = (400,50))
+
+snail_surf = pygame.image.load('Graphics/snail/snail1.png').convert_alpha()
+snail_rect = snail_surf.get_rect(midbottom = (600, 300))
+
+player_surf = pygame.image.load('Graphics/player/player_walk_1.png').convert_alpha()
+player_rect = player_surf.get_rect(midbottom = (80,300))
 
 while True:
     for event in pygame.event.get():
@@ -21,12 +25,24 @@ while True:
             pygame.quit()
             exit()
 
-    screen.blit(sky_surface, (0,0))
-    screen.blit(ground_surface, (0,300))
-    screen.blit(text_surface,(300,50))
-    snail_x_pos -= 4
-    if snail_x_pos < -100: snail_x_pos = 800
-    screen.blit(snail_surface,(snail_x_pos,snail_y_pos))
+    screen.blit(sky_surf, (0,0))
+    screen.blit(ground_surf, (0,300))
+    pygame.draw.rect(screen,('#d0f4f7'),score_rect)
+    pygame.draw.rect(screen,('#d0f4f7'),score_rect,20)
+    screen.blit(score_surf,score_rect)
+    
+    snail_rect.x -= 4
+    if snail_rect.right <= 0: snail_rect.left = 800
+    screen.blit(snail_surf,snail_rect)
+    screen.blit(player_surf,player_rect)
+    
+
+    #if player_rect.collidedict(snail_rect):
+        #print('collision')
+    
+    #mouse_pos = pygame.mouse.get_pos()
+    #if player_rect.collidepoint(mouse_pos):
+        #print('collision')
 
     pygame.display.update()
     clock.tick(60)
