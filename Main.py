3a7808ae017da289baa2f18995ff01570ab5a012
Anthony Esquivel <lawtay90@gmail.com>
2023-03-26 17:11:54 -0500

    Improved readability and added start screen.

--- Main.py
+++ Main.py
@@ -1,18 +1,25 @@
 import pygame
 from sys import exit
 
+def display_score():
+    current_time = int(pygame.time.get_ticks() / 1000) - start_time
+    score_surf = test_font.render(f'Score: {current_time}',False,(64,64,64))
+    score_rect = score_surf.get_rect(center = (400,50))
+    screen.blit(score_surf,score_rect)
+    
 pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Corvids Adventure')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('fonts/Pixeltype.ttf', 50)
-game_active = True
+game_active = False
+start_time = 0
 
 sky_surf = pygame.image.load('Graphics/Sky.png').convert()
 ground_surf = pygame.image.load('Graphics/Ground.png').convert()
 
-score_surf = test_font.render('Corvids Adventure', False, (64,64,64))
-score_rect = score_surf.get_rect(center = (400,50))
+#score_surf = test_font.render('Corvids Adventure', False, (64,64,64))
+#score_rect = score_surf.get_rect(center = (400,50))
 
 snail_surf = pygame.image.load('Graphics/snail/snail1.png').convert_alpha()
 snail_rect = snail_surf.get_rect(midbottom = (600, 300))
@@ -21,29 +28,44 @@ player_surf = pygame.image.load('Graphics/player/player_walk_1.png').convert_alp
 player_rect = player_surf.get_rect(midbottom = (80,300))
 player_gravity = 0
 
-#keys = pygame.key.get_pressed()
-#if keys[pygame.K_SPACE]:
-#    print('Jump')
+#intro screen
+player_stand = pygame.image.load('Graphics/player/player_stand.png').convert_alpha()
+player_stand =pygame.transform.rotozoom(player_stand,0,2)
+player_stand_rect = player_stand.get_rect(center = (400,200 ))
+
+game_name = test_font.render('Corvids Adventure',False,(119,196,169))
+game_name_rect = game_name.get_rect(center =(400,80))
+
+game_mess = test_font.render('Press Space to Continue',False,(119,196,169))
+game_mess_rect = game_name.get_rect(center = (350,330))
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if player_rect.collidepoint(event.pos):
-                player_gravity = -20
-               
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                player_gravity = -20
+        
+        if game_active:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if player_rect.collidepoint(event.pos):
+                    player_gravity = -20
+                
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    player_gravity = -20
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_active = True
+                snail_rect.left = 800
+                start_time = int(pygame.time.get_ticks() / 1000) 
     
     if game_active:      
         screen.blit(sky_surf, (0,0))
         screen.blit(ground_surf, (0,300))
-        pygame.draw.rect(screen,('#d0f4f7'),score_rect)
-        pygame.draw.rect(screen,('#d0f4f7'),score_rect,20)
-        screen.blit(score_surf,score_rect)
+        #pygame.draw.rect(screen,('#d0f4f7'),score_rect)
+        #pygame.draw.rect(screen,('#d0f4f7'),score_rect,20)
+        #screen.blit(score_surf,score_rect)
+        display_score()
         
         snail_rect.x -= 4
         if snail_rect.right <= 0: snail_rect.left = 800
@@ -59,7 +81,10 @@ while True:
         if snail_rect.colliderect(player_rect):
             game_active = False
     else:
-        break
+        screen.fill((94,129,162))
+        screen.blit(player_stand,player_stand_rect)
+        screen.blit(game_name,game_name_rect)
+        screen.blit(game_mess,game_mess_rect)
 
     pygame.display.update()
     clock.tick(60)
